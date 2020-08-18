2ad5bf90203539a276cdef85cca62369addbf5fa
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-18 03:17:49 +0000

    practice of console.assert

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+let stopSign = String.fromCodePoint(128721);
+let checkMark = String.fromCodePoint(0x2705);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(checkMark + checkMark + checkMark + " Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log(stopSign + stopSign + stopSign + " Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
