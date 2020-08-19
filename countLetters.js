dfe1bda2c39eb266cb50f12ba0ff8ade22796c30
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-19 21:58:33 +0000

    complete countLetter module

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+let stopSign = String.fromCodePoint(128721);
+let checkMark = String.fromCodePoint(0x2705);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${checkMark + checkMark + checkMark} Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`${stopSign + stopSign + stopSign} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (words) {
+  let stringArray = [...words];
+  const results = {}
+
+  for (const letter of stringArray) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
