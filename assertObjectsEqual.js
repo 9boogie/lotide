239b920714e62ab3fb531aae07b94b359bc1a886
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-20 02:55:37 +0000

    complete eqobject and assertObject

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -38,16 +38,25 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  if (eqObjects(actual, expected)) {
+    console.log(`${checkMark + checkMark + checkMark} Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`${stopSign + stopSign + stopSign} Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+  }
+};
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "3" };
-assertEqual(eqObjects(ab, ba),true); // => true
+assertObjectsEqual(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc),false); // => false 
+assertObjectsEqual(ab, abc); // => false 
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc),true); // => true
+assertObjectsEqual(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2),false); // => false 
+assertObjectsEqual(cd, cd2); // => false  
