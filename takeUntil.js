1d5461a589f20c02121cae68791629f82c05e0e8
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-21 03:05:21 +0000

    complete takeUntil.js module

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const takeUntil = function(array, callback) {
+  let outcome = [];
+  for (let element of array) {
+    if (callback(element)) {
+      break;
+    } else {
+      outcome.push(element);
+    }
+  } return outcome;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log("The two arrays are equal.");
+  } else {
+    console.log("The two arrays are not equal.")
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+      } else {
+        return false;
+      } 
+    } return true;
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
