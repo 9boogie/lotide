6da96d181da9724e7ba499ff959b90f2a0a3f27c
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-24 20:42:34 +0000

    Refactor applied

--- tail.js
+++ tail.js
@@ -1,6 +1,3 @@
-let stopSign = String.fromCodePoint(128721);
-let checkMark = String.fromCodePoint(0x2705);
-
 const tail = function(array) {
   let newArray = [];
   if (array === undefined) {
@@ -16,18 +13,4 @@ const tail = function(array) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${checkMark + checkMark + checkMark} Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`${stopSign + stopSign + stopSign} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
