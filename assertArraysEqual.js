443ad8c24498363ecd530d843862155730df88a6
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-24 21:02:35 +0000

    Refactor applied

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,4 @@
-let stopSign = String.fromCodePoint(128721);
-let checkMark = String.fromCodePoint(0x2705);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${checkMark + checkMark + checkMark} Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`${stopSign + stopSign + stopSign} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1,arr2)) {
@@ -17,19 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-      } else {
-        return false;
-      } 
-    } return true;
-  }
-};
-
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,3 @@
-let stopSign = String.fromCodePoint(128721);
-let checkMark = String.fromCodePoint(0x2705);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${checkMark + checkMark + checkMark} Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`${stopSign + stopSign + stopSign} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-/*
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-      } else {
-        return false;
-      } 
-    } return true;
-  }
-};*/
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -35,6 +11,4 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+module.exports = eqArrays;
