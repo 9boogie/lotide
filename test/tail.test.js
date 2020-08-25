aca7f4e7ee5de6e8a5e78c77c21653ea44d2e2a1
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-25 00:04:43 +0000

    Mocha Chai test

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,7 +1,23 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("Ensure we get back two elements for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+
+  it("Ensure first element is \"Lighthouse\" for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+
+  it("Ensure last element is \"Labs\" for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+});
